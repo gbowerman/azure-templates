92971fd56642f6b0b640c5232ab89e5656b77d99
Guy Bowerman <guybo@outlook.com>
2017-05-27 15:39:30 -0700

    Update to latest API versions

--- apiUpgrader.py
+++ apiUpgrader.py
@@ -1,23 +1,23 @@
 # Python script to update API versions in Azure Resource Manager templates and make simple compatibility updates
-# 
+#
 # Usage: python apiUpgrader.py source_template.json > output_template.json
 #
 # Note: The script won't catch everything, for example extensions which make references based on earlier APIs.
 # The script will also make mistakes if your source template sets advanced properties that the script didn't check for.
 # Thereforee check the output manually and test before relying on the output scripts.
-# 
+#
 # Author: guybo@outlook.com
-# 
-# Last update 9/17/2016
+#
+# Last update 5/27/2017
 
+from collections import OrderedDict
 import json
 import sys
 import os.path
-from collections import OrderedDict
 
 # update this list to current versions before running the script
-computeApiVersion = '2016-03-30'
-networkApiVersion = '2016-06-01'
+computeApiVersion = '2017-03-30'
+networkApiVersion = '2017-04-01'
 storageApiVersion = '2016-01-01'
 insightsApiVersion = '2015-04-01'
 
@@ -29,10 +29,12 @@ containsNetworkResource = False
 containsStorageResource = False
 containsInsightsResource = False
 
+
 def usage():
     sys.exit('Usage: python ' + sys.argv[0] + ' filename')
 
-# check for single command argument representing template filename   
+
+# check for single command argument representing template filename
 if len(sys.argv) != 2:
     usage()
 
@@ -45,15 +47,16 @@ if os.path.isfile(templateFileStr) is False:
 
 # Load JSON template file
 try:
-   with open(templateFileStr) as templateFile:    
-      templateData = json.load(templateFile, object_pairs_hook=OrderedDict)
+    with open(templateFileStr) as templateFile:
+        templateData = json.load(templateFile, object_pairs_hook=OrderedDict)
 except FileNotFoundError:
-   print("Error: " + templateFile + " file not found.")
-   sys.exit()
+    print("Error: " + templateFile + " file not found.")
+    sys.exit()
 
 # templateData is now loaded with the ARM template, with order preserved
 
-# loop through resources setting current API versions and making any compatibility upgrades
+# loop through resources setting current API versions and making any
+# compatibility upgrades
 for resource in templateData['resources']:
     if resource['type'].startswith('Microsoft.Compute'):
         containsComputeResource = True
@@ -76,7 +79,7 @@ for resource in templateData['resources']:
     elif resource['type'].startswith('Microsoft.Insights'):
         containsInsightsResource = True
         resource['apiVersion'] = "[variables('insightsApiVersion')]"
-        
+
 # create or overwrite variables for the resource API versions if they exist
 if containsComputeResource == True:
     templateData['variables']['computeApiVersion'] = computeApiVersion
@@ -86,6 +89,6 @@ if containsStorageResource == True:
     templateData['variables']['storageApiVersion'] = storageApiVersion
 if containsInsightsResource == True:
     templateData['variables']['insightsApiVersion'] = insightsApiVersion
-    
+
 # print the upgraded JSON template to standard out
 print(json.dumps(templateData, sort_keys=False, indent=2, separators=(',', ': ')))
